309d88709ec96227e4206efa021b95d06bd204c8
Sachin Gurjar <sachingurjar8109@gmail.com>
2025-01-09 04:20:03 +0530

    done

--- src/flags/index.ts
+++ src/flags/index.ts
@@ -3,19 +3,25 @@ const countryList = getCountries();
 
 const loadFlag = (countryCode: string) => {
   try {
-    return require(`./${countryCode}.svg`);
+    const flag = require(`./${countryCode}.svg`);
+    return flag;
   } catch (error) {
     console.error(`Error loading flag for ${countryCode}:`, error);
     return null;
   }
 };
+const loadAllFlags = () => {
+  const flags: Record<string, string> = {};
+  countryList.forEach((countryCode) => {
+    const flagImage = loadFlag(countryCode);
+    if (flagImage) {
+      flags[countryCode] = flagImage;
+    }
+  });
 
-const flags = countryList.reduce((acc: any, countryCode) => {
-  const flagImage = loadFlag(countryCode);
-  if (flagImage) {
-    acc[countryCode] = flagImage;
-  }
-  return acc;
-}, {});
+  return flags;
+};
+
+const flags = loadAllFlags();
 
 export { flags };
