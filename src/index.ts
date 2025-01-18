b774cf6e38b5bce3a5135f9e00f038d303111162
Sachin Gurjar <sachingurjar8109@gmail.com>
2025-01-19 03:34:14 +0530

    export updated

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as PhoneNumber } from "./components/PhoneNumber";
 export { default as usePhoneNumberInput } from "./utils/usePhoneNumberInput";
+export { getCountries, getCountry } from "./countries";
